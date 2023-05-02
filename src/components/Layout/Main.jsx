061fa400380a84021247a8971a078ae5e1fa5773
rohan-haque <rohanhaque234@gmail.com>
2023-05-02 10:29:42 +0600

    Footer created and responsive design added on it

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../Header/Navbar';
+import Footer from '../Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
             <Navbar />
+            <Outlet />
+            <Footer />
         </div>
     );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../components/Layout/Main";
+import Home from "../components/Layout/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main />
+    element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
   },
 ]);
 
