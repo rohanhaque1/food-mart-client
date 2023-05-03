94bf06cdebd5f3c73ad67545ca6443efa761a99d
rohan-haque <rohanhaque234@gmail.com>
2023-05-03 13:38:00 +0600

    Our food section added as an extra section in homepage

---
+++ src/Pages/OurFoods.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Food from "./Food";
+
+const OurFoods = () => {
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/foods")
+      .then((res) => res.json())
+      .then((data) => setFoods(data));
+  }, []);
+
+  return (
+    <>
+      <section className="px-8 md:px-20 py-14 mx-auto">
+        <div className="text-center space-y-3 my-4 w-2/5 mx-auto">
+          <h2 className="text-3xl font-bold">The Best Italian Foods</h2>
+          <p className="text-md font-semibold text-gray-500 mb-5">
+            Pizza Margherita is perhaps the most famous Italian food. Wherever
+            you go in the world, you'll never be too far from a Margherita
+            pizza.
+          </p>
+        </div>
+        <div className="grid grid-cols-3 gap-6">
+          {foods.map((food) => (
+            <Food key={food.id} food={food}></Food>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default OurFoods;
--- src/components/Layout/Home.jsx
+++ src/components/Layout/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../../Pages/Banner';
 import Chefs from '../../pages/Chefs';
+import OurFoods from '../../pages/OurFoods';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <OurFoods />
             <Chefs />
         </div>
     );
