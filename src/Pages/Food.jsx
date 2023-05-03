e04a16b80ea46a80b92cf4096139b8644755ef6e
rohan-haque <rohanhaque234@gmail.com>
2023-05-03 15:36:18 +0600

    data displayed to the food section

--- src/Pages/Food.jsx
+++ src/Pages/Food.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
-const Food = () => {
+const Food = ({ food }) => {
+    const {name, picture, ingredients, review } = food;
     return (
-        <div>
+      <>
+        <div className="card bg-white shadow-xl">
+          <figure>
+            <img src={picture} alt="food" className="h-96" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title font-bold">
+              {name}
+              <div className="badge badge-warning">{review ? review : "No review"}</div>
+            </h2>
+            <p className="font-bold">Ingredients :</p>
+            <ul>{ingredients.map((item, index) => <li key={index}><span>{index + 1}. </span>{item}</li>)}</ul>
             
+          </div>
         </div>
+      </>
     );
 };
 
