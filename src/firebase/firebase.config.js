d643febf8c69bba82bae7c082f8a2c9c69415f17
rohan-haque <rohanhaque234@gmail.com>
2023-05-04 13:50:54 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('environment', import.meta.env.VITE_PASS)
 const firebaseConfig = {
-  apiKey: "AIzaSyCUVEiUnMY0bYBnWO_PMyHXkFnWGFWgFUc",
-  authDomain: "food-recipe-client-5a145.firebaseapp.com",
-  projectId: "food-recipe-client-5a145",
-  storageBucket: "food-recipe-client-5a145.appspot.com",
-  messagingSenderId: "800298351763",
-  appId: "1:800298351763:web:e8ebfbbd0cda948af42166",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
